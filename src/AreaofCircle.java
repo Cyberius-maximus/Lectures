fd9d69f3d68ae538bc0948377ba0856187561922
Cyberius-maximus <148473976+Cyberius-maximus@users.noreply.github.com>
2023-11-06 16:46:56 +0100

    LectureCommit

---
+++ src/AreaofCircle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class AreaofCircle {
+ 
+
+    public static void main(String[] args) 
+    {
+        int r;
+        double pi = 3.142, area;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter radius of circle: ");
+        r = s.nextInt();
+        area = pi * r * r;
+        System.out.println("Area of circle: "+area);
+         s.close();
+   
+    }            
+
+
+    
+}
---
+++ src/ArtihmetiOp2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;// universalle klassen mit extra funktionen damit man sie nicht jedesmal selber programmieren muss  online kann man dan nachschauen welche methoden usw in den klassen sind.
+public class ArtihmetiOp2 {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);// damit erstellt man eine scanner klasse
+    System.out.print("Enter the first number: ");
+    int n1 = scanner.nextInt();
+    System.out.print("Enter the second number: ");
+    int n2 = scanner.nextInt();
+    scanner.close();
+    System.out.println("Addition: " + (n1+n2));
+    System.out.println("Subtraction: " + (n1-n2));
+    System.out.println("Multiplication: " + (n1*n2));
+    System.out.println("Division: " + (n1/n2));
+    System.out.println("Modulus: " + (n1%n2));
+    System.out.println("Exponentiation : " + (n1^n2));
+
+
+}
+}
