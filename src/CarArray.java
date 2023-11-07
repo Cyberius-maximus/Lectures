eba2632e94dd5e090b5d76242d6fa448211020b6
Cyberius-maximus <148473976+Cyberius-maximus@users.noreply.github.com>
2023-11-07 11:31:49 +0100

    Tutorial4

---
+++ src/CarArray.java
@@ -0,0 +1,15 @@
+public class CarArray {
+    public static void main(String[] args) {
+        String[][] cars = {
+            {"Ferrai", "Red"},
+            {"BMW", "Blue"},
+            {"Mercedes", "Black"},
+            {"Lambo", "Yellow"}
+        };
+
+        System.out.println("Available car brands and colors:");
+        for (String[] car : cars) {
+            System.out.println("Brand: " + car[0] + ", Color: " + car[1]);
+        }
+    }
+}
